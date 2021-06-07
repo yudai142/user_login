ccd33b0e2ef6219eb75654c0f3d0163443029ccd
yudaishimoji42865233 <yudai.shimoji.42865233@gmail.com>
2021-06-07 21:00:47 +0900

    新規登録時のCSRF対策実装

---
+++ functions.php
@@ -0,0 +1,29 @@
+<?php
+  /**
+   * XSS対策：エスケープ処理
+   * htmlspecialchars()を使うことで対策可能
+   * @param string $str 対象の文字列
+   * @return string 処理された文字列
+   */
+  function h($str) {
+    return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+  }
+
+  /**
+   * CSRF対策(偽装リクエスト対策)
+   * ワンタイムトークンを利用して対策
+   * @param void
+   * @return string $csrf_token
+   */
+  function setToken() {
+    // トークンを生成
+    // フォームからそのトークンを送信
+    // 送信後の画面でそのトークンを照会
+    // トークンを削除
+    session_start();
+    $csrf_token = bin2hex(random_bytes(32));
+    $_SESSION['csrf_token'] = $csrf_token;
+
+    return $csrf_token;
+  }
+?>
